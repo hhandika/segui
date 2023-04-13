28a6e76c5151c0d2eeecc2e58635057d4c2d8181
Heru Handika <herubiolog@gmail.com>
2023-04-13 13:22:04 -0500

    Add macos intallation scripts.

--- scripts/codegen.sh
+++ scripts/codegen.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 flutter_rust_bridge_codegen --rust-input api/src/api.rs --dart-output lib/bridge_generated.dart --dart-decl-output lib/bridge_definitions.dart
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cargo install flutter_rust_bridge_codegen
+flutter pub add --dev ffigen && flutter pub add ffi
+cargo install cargo-xcode
