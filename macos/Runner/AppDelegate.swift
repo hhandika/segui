f0d64c2cc2594108769d88e7ab31bfeb7597dcfc
Heru Handika <herubiolog@gmail.com>
2023-04-17 09:13:42 -0500

    Connect rust to macos and ios.

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,10 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+    
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    let dummy = dummy_method_to_enforce_bundling()
+    print(dummy)
     return true
   }
 }
