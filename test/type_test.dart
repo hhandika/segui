a96acd111f574939310d4e6ac92410e88dea2398
Heru Handika <herubiolog@gmail.com>
2023-05-03 16:42:53 -0500

    Add table test.

---
+++ test/type_test.dart
@@ -0,0 +1,20 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:segui/services/types.dart';
+
+void main() {
+  test('Test NCBI table map', () {
+    // Get index of "Standard Code" in NCBI table
+    final int index = translationTable.indexOf("Standard Code");
+    // Get NCBI table map
+    final String? indexStr = translationTableMap[index];
+
+    expect(indexStr, "1");
+  });
+}
