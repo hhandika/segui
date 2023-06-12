3e1933720b32ddd29cc56e5e87a8b228f3d58aff
Heru Handika <herubiolog@gmail.com>
2023-06-12 13:50:54 -0500

    Update ffi.

--- lib/services/native.dart
+++ lib/services/native.dart
@@ -1,8 +1,13 @@
 import 'dart:ffi';
 import 'dart:io';
 
-export 'package:segui/bridge_definitions.dart';
-import 'package:segui/bridge_generated.dart';
+export 'package:segui/bridge_generated.dart';
+
+import '../bridge_generated.dart';
+// import '../bridge_definitions.dart';
+
+// export '../bridge_definitions.dart';
+// export 'package:segui/bridge_generated.dart';
 
 const _base = 'segul_api';
 
