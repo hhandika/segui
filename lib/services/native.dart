da5420e7aae11998670099504b33c64bd7568d20
Heru Handika <herubiolog@gmail.com>
2023-06-13 20:47:59 -0500

    Export bridge definitions.

--- lib/services/native.dart
+++ lib/services/native.dart
@@ -1,13 +1,10 @@
 import 'dart:ffi';
 import 'dart:io';
 
-export 'package:segui/bridge_generated.dart';
-
 import '../bridge_generated.dart';
-// import '../bridge_definitions.dart';
 
-// export '../bridge_definitions.dart';
-// export 'package:segui/bridge_generated.dart';
+export 'package:segui/bridge_generated.dart';
+export '../bridge_definitions.dart';
 
 const _base = 'segul_api';
 
