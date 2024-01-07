9d775c0c0d4397ebdf8b533c0fe0247a3aed7a55
Heru Handika <herubiolog@gmail.com>
2024-01-07 14:37:27 -0600

    Updat descirption.

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={siteConfig.tagline}
+      description="SEGUL is an ultra-fast, lightweight tool for phylogenomics."
     >
       <HomepageHeader />
       <main>
