d15751bb4e9e26f408ab6ee01715e4468f387e98
Heru Handika <herubiolog@gmail.com>
2024-01-19 17:24:49 -0600

    Update doc link.

---
+++ lib/screens/alignment/partition.dart
@@ -0,0 +1,19 @@
+/// Convert partition to a different format
+import 'package:flutter/material.dart';
+
+
+
+
+class  extends StatefulWidget {
+  const ({super.key});
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
