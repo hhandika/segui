d1bf9023bc7d68591f236af789ef199f008bf6f0
Heru Handika <herubiolog@gmail.com>
2024-01-22 17:30:31 -0600

    Only log to file.

--- rust/src/api/common.rs
+++ rust/src/api/common.rs
@@ -9,5 +9,5 @@ pub fn init_logger(log_dir: String) {
     let current_date = chrono::Local::now().format("%Y-%m-%d").to_string();
     let file = format!("{}_{}.log", LOG_FILE, current_date);
     let log_path = Path::new(&log_dir).join(file);
-    logger::init_logger(&log_path).expect("Failed to initialize logger");
+    logger::init_file_logger(&log_path).expect("Failed to initialize logger");
 }
