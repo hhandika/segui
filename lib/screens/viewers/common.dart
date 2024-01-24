f881ef8087492989269c952613f6ca4f7b7ac52b
Heru Handika <herubiolog@gmail.com>
2024-01-24 12:09:49 -0600

    Clean module name.

--- lib/screens/viewers/common.dart
+++ lib/screens/viewers/common.dart
@@ -1,7 +1,7 @@
 import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:segui/screens/viewers/tabulated.dart';
-import 'package:segui/screens/viewers/plain_text.dart';
+import 'package:segui/screens/viewers/text.dart';
 import 'package:segui/services/io.dart';
 
 class OpenViewerButton extends StatelessWidget {
