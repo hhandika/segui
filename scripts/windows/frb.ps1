97ed72dfed05ad88072f13c0c89c41dea05c1b61
Heru Handika <herubiolog@gmail.com>
2024-02-17 17:58:44 -0600

    Update windows frb script.

--- scripts/windows/frb.ps1
+++ scripts/windows/frb.ps1
@@ -1,4 +1,22 @@
 rm -r ./lib/src/rust/api/frb_*
 rm -r ./rust/frb_*
 
-flutter_rust_bridge_codegen.exe generate
+echo "Cleaning Flutter build files"
+flutter clean
+
+echo "Updating Rust dependencies"
+cd rust
+cargo update
+cd ..
+
+echo "Updating FRB"
+cargo install 'flutter_rust_bridge_codegen@^2.0.0-dev.0'
+
+echo "Updating Dart dependencies"
+flutter pub upgrade
+
+echo "Generating FRB for Dart"
+flutter_rust_bridge_codegen generate
+
+echo "Running Dart fix"
+dart fix --apply
