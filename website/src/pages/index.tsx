7f6623540b75e0799383bd762137431460c29562
Heru Handika <herubiolog@gmail.com>
2024-04-23 13:43:57 -0500

    Fix button links and update GUI/CLI support.

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--outline button--primary button--lg"
-            to="/docs/installation/overview"
+            to="/docs/intro/"
           >
             Learn SEGUL
           </Link>
