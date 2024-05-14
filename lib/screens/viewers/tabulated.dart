b843ca6922ec6de31d7e42ec1fa23b963efba1e5
Heru Handika <herubiolog@gmail.com>
2024-05-13 21:23:03 -0500

    Add support for paginated table.

--- lib/screens/viewers/tabulated.dart
+++ lib/screens/viewers/tabulated.dart
@@ -5,6 +5,7 @@ import 'package:path/path.dart';
 import 'package:segui/screens/shared/buttons.dart';
 import 'package:segui/screens/shared/common.dart';
 import 'package:segui/screens/shared/io/io.dart';
+import 'package:segui/screens/shared/pages.dart';
 import 'package:segui/services/viewers/csv.dart';
 import 'package:segui/styles/decoration.dart';
 
@@ -27,11 +28,13 @@ class TabulatedFileView extends StatelessWidget {
       body: Center(
         child: Padding(
             padding: const EdgeInsets.all(16),
-            child: Container(
-                decoration: getContainerDecoration(context),
-                child: Column(
-                  children: [
-                    Padding(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 4),
+                  child: Container(
+                    decoration: getContainerDecoration(context),
+                    child: Padding(
                       padding: const EdgeInsets.all(16),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -42,14 +45,17 @@ class TabulatedFileView extends StatelessWidget {
                         ],
                       ),
                     ),
-                    const TopDivider(),
-                    Expanded(
-                        child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: TabulatedFileViewBody(file: file),
-                    )),
-                  ],
-                ))),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: getContainerDecoration(context),
+                    child: TabulatedFileViewBody(file: file),
+                  ),
+                ),
+              ],
+            )),
       ),
     );
   }
@@ -75,27 +81,11 @@ class _TabulatedFileViewBodyState extends State<TabulatedFileViewBody> {
             if (snapshot.data!.isEmpty) {
               return const Center(child: Text('Failed to parse file.'));
             }
-            return content!.length > 300
-                ? BigFileErrors(file: widget.file)
-                : SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    clipBehavior: Clip.antiAlias,
-                    child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        clipBehavior: Clip.antiAlias,
-                        child: DataTable(
-                          columns: content[0]
-                              .map((e) => DataColumn(label: Text(e.toString())))
-                              .toList(),
-                          rows: content
-                              .sublist(1)
-                              .map((e) => DataRow(
-                                  cells: e
-                                      .map((e) => DataCell(Text(e.toString())))
-                                      .toList()))
-                              .toList(),
-                        )),
-                  );
+            return content != null
+                ? PaginatedTable(content: content)
+                : const EmptyScreen(
+                    title: 'Table is empty.',
+                    description: 'Try run the analysis again.');
           } else {
             return const Center(child: CircularProgressIndicator());
           }
@@ -130,4 +120,68 @@ class BigFileErrors extends StatelessWidget {
       ),
     );
   }
+}
+
+class PaginatedTable extends StatefulWidget {
+  const PaginatedTable({super.key, required this.content});
+
+  final List<List<dynamic>> content;
+
+  @override
+  State<PaginatedTable> createState() => _PaginatedTableState();
+}
+
+class _PaginatedTableState extends State<PaginatedTable> {
+  @override
+  Widget build(BuildContext context) {
+    int screenHeight = MediaQuery.of(context).size.height.toInt();
+    return SingleChildScrollView(
+        child: Theme(
+      data: Theme.of(context).copyWith(
+          cardTheme: const CardTheme(elevation: 0, color: Colors.transparent)),
+      child: PaginatedDataTable(
+        rowsPerPage: _rowCount(screenHeight),
+        columns: widget.content[0]
+            .map((e) => DataColumn(label: Text(e.toString())))
+            .toList(),
+        // We skip the first row, which is the header
+        source: ContentSource(widget.content.sublist(1)),
+      ),
+    ));
+  }
+
+  int _rowCount(int screenHeight) {
+    if (widget.content.length <= 50) {
+      return widget.content.length;
+    } else {
+      // Estimate row number from screen height
+      // 58 is the average row height
+      return (screenHeight * .88) ~/ 56;
+    }
+  }
+}
+
+class ContentSource extends DataTableSource {
+  ContentSource(this.content);
+
+  final List<List<dynamic>> content;
+
+  @override
+  DataRow? getRow(int index) {
+    if (index >= content.length) {
+      return null;
+    }
+    return DataRow(
+        cells:
+            content[index].map((e) => DataCell(Text(e.toString()))).toList());
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => content.length;
+
+  @override
+  int get selectedRowCount => 0;
 }
