b4007ee14708bd6c2e3cdf9c3ee25b4e79b71011
Heru Handika <herubiolog@gmail.com>
2024-05-29 20:16:04 -0500

    Add feature lists and cleaned front page layout.

--- website/src/components/DownloadButtons/index.tsx
+++ website/src/components/DownloadButtons/index.tsx
@@ -6,10 +6,10 @@ function WindowsBadge(): JSX.Element {
   return (
     <Link to="https://apps.microsoft.com/detail/SEGUI/9np1bq6fw9pw?mode=direct/">
       <img
-        className="margin--md"
+        className="margin-horiz--none margin-bottom--none"
         alt="Get it on Microsoft Store"
         src="https://get.microsoft.com/images/en-us%20dark.svg"
-        width="180"
+        width="160"
       />
     </Link>
   );
@@ -21,7 +21,7 @@ function GooglePlayBadge(): JSX.Element {
       <img
         alt="Get it on Google Play"
         src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-        height="80"
+        height="72"
       />
     </Link>
   );
@@ -31,10 +31,10 @@ function MacStoreBadge(): JSX.Element {
   return (
     <Link to="https://apps.apple.com/us/app/segui/id6447999874?mt=12&amp;itsct=apps_box_badge&amp;itscg=30200">
       <img
-        className="margin--md"
+        className="margin-horiz--sm margin-bottom--none"
         src="https://tools.applemediaservices.com/api/badges/download-on-the-mac-app-store/black/en-us?size=250x83&amp;releaseDate=1716076800"
         alt="Download on the Mac App Store"
-        width="254"
+        width="200"
       />
     </Link>
   );
@@ -44,10 +44,10 @@ function AppStoreBadge(): JSX.Element {
   return (
     <Link to="https://apps.apple.com/us/app/segui/id6447999874?itsct=apps_box_badge&amp;itscg=30200">
       <img
-        className="margin--sm"
+        className="margin-horiz--none margin-bottom--sm"
         src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1716076800"
         alt="Download on the App Store"
-        width="180"
+        width="160"
       />
     </Link>
   );
@@ -57,14 +57,13 @@ function DownloadOptions() {
   return (
     <div className={styles.install}>
       <Heading as="h2">Install SEGUL GUI</Heading>
-      <p>
+      <p className="text--no-decoration">
         <WindowsBadge />
         <MacStoreBadge />
       </p>
-      <Heading as="h3">Mobile Version</Heading>
       <AppStoreBadge />
       <GooglePlayBadge />
-      <p>
+      <p className="padding--none">
         <Link to="/docs/installation/overview">
           CLI & other install options
         </Link>
--- website/src/components/FeatureLists/index.tsx
+++ website/src/components/FeatureLists/index.tsx
@@ -1,59 +1,67 @@
 import clsx from "clsx";
 import Heading from "@theme/Heading";
 
-const AlignmentFeatureLists: string[] = [
-  "Concatenation",
-  "Conversion",
-  "Filtering",
-  "Partition conversion",
-  "Splitting",
-  "Summarization",
-];
+type SupportedItem = {
+  title: string;
+  description: string;
+};
 
-const GenomicFeatureLists: string[] = [
-  "Raw read summarization",
-  "Contiguous sequence summarization",
+const SupportedList: SupportedItem[] = [
+  {
+    title: "Alignments",
+    description:
+      "Concatenate, convert, filter, split, and summarize alignment datasets. \
+      Also convert between different partition formats. \
+      Support Sanger and next-generation sequencing data.",
+  },
+  {
+    title: "Genomics",
+    description:
+      "Summarize FASTQ read and contiguous sequence datasets. \
+      Work on multiple large datasets simultaneously.",
+  },
+  {
+    title: "Sequences",
+    description:
+      "Extract, map, rename, remove, and translate sequence datasets. \
+      Support Sanger and next-generation sequencing data.",
+  },
 ];
-
-const SequenceFeatureLists: string[] = [
-  "Extraction",
-  "Unique ID extraction",
-  "ID mapping",
-  "ID renaming",
-  "Removal",
-  "Translation",
-];
-
-const FeatureHeader: string[] = ["Alignment", "Genomic", "Sequence"];
 
 // List all feature in tables with "Features" as heading
 // and the features in the list
-function SupportedFeatures(
-  { heading }: { heading: string },
-  features: string[]
-): JSX.Element {
+function SupportedFeatures({ title, description }: SupportedItem): JSX.Element {
   return (
-    <div>
-      <Heading as="h2">{heading}</Heading>
-      <table>
-        <tbody>
-          {features.map((feature) => (
-            <tr key={feature}>
-              <td>{feature}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className={clsx("col")}>
+      <div className="card">
+        <div className="card__header text--primary">
+          <Heading as="h3">{title}</Heading>
+        </div>
+        <div className="card__body">
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default function SupportedFeatureList(): JSX.Element {
+export default function SupportedFeatureLists(): JSX.Element {
   return (
     <section>
-      {SupportedFeatures({ heading: "Alignment" }, AlignmentFeatureLists)}
-      {SupportedFeatures({ heading: "Genomic" }, GenomicFeatureLists)}
-      {SupportedFeatures({ heading: "Sequence" }, SequenceFeatureLists)}
+      <div className="container">
+        <div className="text--center">
+          <Heading as="h2">Features</Heading>
+          <p>
+            SEGUL features operation on alignment, genomic-specific, and
+            sequence datasets.
+          </p>
+        </div>
+        <div className="row">
+          {SupportedList.map((props, idx) => (
+            <SupportedFeatures key={idx} {...props} />
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
