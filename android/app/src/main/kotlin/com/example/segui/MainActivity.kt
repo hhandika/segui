c9488c4c56782614b1fbc88cb528b5612ec24837
Heru Handika <herubiolog@gmail.com>
2025-07-27 21:25:16 -0400

    Migrate graddle.

---
+++ android/app/src/main/kotlin/com/example/segui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.segui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/hhandika/segui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.hhandika.segui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
